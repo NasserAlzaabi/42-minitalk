2fadafbbb23fee19b0197e905372b79de414108c
NasserAlzaabi <Nasseralzaabi03@gmail.com>
2023-06-06 11:42:59 +0400

    update

--- client.c
+++ client.c
@@ -4,12 +4,17 @@ void clientHandler(){ //what to do when signal is recieved with server PID
 
 }
 
-int main(int argc, char** argv){ //argv[1] is PID input argv[2] is string input
-	printf("%s", argv[1]);
-	int x = atoi(argv[1]);
-	if (argc == 3){
-		kill(x , SIGUSR1);
+void chartobit(char c){
+	for (int i = 7; i >= 0; i--){
+		putchar ((c & (1 << i)) ? '1' : '0');
 	}
-	printf("does this even run");
+}
+
+int main(int argc, char** argv){ //argv[1] is PID input argv[2] is string input
+	chartobit(argv[1][0]);
+	// if (argc == 3){
+	// 	//kill(x , SIGUSR1);
+	// }
+	// printf("does this even run");
 	return 0;
 }
--- server.c
+++ server.c
@@ -14,5 +14,8 @@ void serverHandler(){
 int main(){
 	
 	printf("The server PID is: %d", getID());
-	
+	signal(SIGUSR1, serverHandler);
+	signal(SIGUSR2, serverHandler);
+	while(1)
+		pause();
 }
